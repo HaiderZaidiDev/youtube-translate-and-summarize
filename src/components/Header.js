203b5a174fc65b886c43976cc8989c38fe71c80e
Haider Zaidi <contact@haiderzaidi.ca>
2022-06-16 18:38:08 -0400

    Added link to see-code button

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
             <Col xs={6}>
                 <h1> Translate and Summarize Youtube Videos</h1>
                 <p> YouTube videos can be long and difficult to watch - many don’t have captions and are in varying languages. In an effort to make the world smaller and increase accessibility of information, this platform can translate and summarize videos for you.</p>
-                <button type="text" className="btn-cta"> See the Code </button>
+                <button type="text" className="btn-cta"> <a href="https://github.com/HaiderZaidiDev/youtube-translate-and-summarize" target="_blank">See the Code</a></button>
             </Col>
             <Col xs={6} className="header-icon">
                 <img src={VideoIcon}/><img/>
